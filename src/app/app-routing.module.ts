08af24d9cbb14a908462a86aac4c1e7979bb91be
mmoragu <58309060+mmoragu@users.noreply.github.com>
2023-12-03 21:00:46 +0100

    navegation pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:PortfolioComponent, pathMatch:'full'},
+  {path:'about', component:AboutComponent, pathMatch:'full'},
+  {path:'item',component:ItemComponent, pathMatch:'full'},
+  { path: '**', component: PortfolioComponent ,pathMatch : 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
