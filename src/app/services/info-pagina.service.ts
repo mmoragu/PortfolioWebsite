709e39047dea3a5bdebe3c20ed6bbc797c974a8c
mmoragu <58309060+mmoragu@users.noreply.github.com>
2023-12-04 22:14:55 +0100

    add example service

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor() { 
+    console.log("servicio de infoPagina listo");
+  }
+}
