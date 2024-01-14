448818acc6f029e1c34b2c0d8affbb3aff704ee1
mmoragu <58309060+mmoragu@users.noreply.github.com>
2024-01-14 12:40:30 +0100

    add call to firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,10 +10,30 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada: boolean = false;
 
+  equipo!: any[];
+
   constructor(private httpClient: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+
     this.httpClient.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-    })
+    });
+
+  }
+
+  private cargarEquipo() {
+
+    this.httpClient.get('https://angular-html-1fffe-default-rtdb.firebaseio.com/equipo.json').subscribe((resp: any) => {
+      this.cargada = true;
+      this.equipo = resp;
+    });
+
   }
 }
